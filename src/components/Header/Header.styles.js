738109e278c401faa194c51a2da9f3a0f2ee5da2
Andrew Zhou <andrew.zhou93@gmail.com>
2021-11-12 01:23:45 -0800

    hotfix: navbar css for tablets

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -53,6 +53,18 @@ export const Navbar = styled.div`
         padding: 15px;
         height: calc(var(--iconHeight) - 10px);
       }
+
+      @media (pointer: coarse) and (orientation: landscape) {
+        background-color: black;
+        padding: 10px;
+        height: calc(var(--iconHeight) - 10px);
+      }
+      @media (pointer: coarse) and (min-width: 400px) {
+        background-color: black;
+        padding: 0px 30px;
+        height: calc(var(--iconHeight));
+        width: 25vw;
+      }
     }
   }
 
@@ -73,6 +85,18 @@ export const Navbar = styled.div`
       height: calc(var(--iconHeight) - 10px);
       justify-content: space-around;
     }
+
+    @media (pointer: coarse) and (orientation: landscape) {
+      /* background-color: green; */
+      height: calc(var(--iconHeight) - 10px);
+      justify-content: space-around;
+    }
+
+    @media (pointer: coarse) and (min-width: 400px) {
+      /* background-color: green; */
+      height: calc(var(--iconHeight));
+      justify-content: space-around;
+    }
   }
   // Each Icon + Title
   li {
@@ -90,6 +114,27 @@ export const Navbar = styled.div`
       width: 7.5vw;
       height: calc(var(--iconHeight) - 10px);
     }
+
+    @media (pointer: coarse) and (orientation: landscape) {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      flex-grow: 0;
+      justify-content: space-between;
+      width: 7.5vw;
+      height: calc(var(--iconHeight) - 10px);
+    }
+
+    @media (pointer: coarse) and (min-width: 400px) {
+      /* background-color: green; */
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      flex-grow: 0;
+      justify-content: space-between;
+      width: 17vw;
+      height: calc(var(--iconHeight));
+    }
   }
   // Icon Only
   img {
@@ -98,6 +143,18 @@ export const Navbar = styled.div`
     margin-right: 5px;
     @media (pointer: fine) {
       width: calc(var(--iconHeight) - 10px);
+    }
+
+    @media (pointer: coarse) and (orientation: landscape) {
+      /* background-color: black; */
+      width: calc(var(--iconHeight) - 10px);
+    }
+
+    @media (pointer: coarse) and (min-width: 400px) {
+      /* background-color: black; */
+      width: 7vw;
+      padding-right: 2px;
+      margin-right: 5px;
     }
   }
 `;
